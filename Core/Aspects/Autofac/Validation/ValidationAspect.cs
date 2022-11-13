085b172658a765d94b37c580c2c167d12f024568
Ahmet ALTINTAŞ <ahmtaltnts35@gmail.com>
2022-11-13 10:14:21 +0300

    13,11,2022

--- Core/Aspects/Autofac/Validation/ValidationAspect.cs
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -13,6 +13,7 @@ namespace Core.Aspects.Autofac.Validation
     public class ValidationAspect : MethodInterception
     {
         private Type _validatorType;
+        
         public ValidationAspect(Type validatorType)
         {
             if (!typeof(IValidator).IsAssignableFrom(validatorType))
--- Core/CrossCuttingConcerns/Validation/ValidationTool.cs
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -3,12 +3,10 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
-using ValidationException = FluentValidation.ValidationException;
 
 namespace Core.CrossCuttingConcerns.Validation
 {
